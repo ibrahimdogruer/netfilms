72a1e9a931c0570fa47a58a887aa5f62ba4b59ed
Ibrahim Dogruer <ibrahim.dogruer.97@gmail.com>
2023-05-17 17:55:56 +0300

    added header and footer components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,21 @@
-import '@/styles/reset.css'
-import '@/styles/global.css'
+import { Inter } from "@next/font/google";
+
+import "@/styles/reset.css";
+import "@/styles/global.css";
+
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+const interFontFamily = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head />
-      <body>{children}</body>
+    <html lang="tr" className={interFontFamily.className}>
+      <body className="container">
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
